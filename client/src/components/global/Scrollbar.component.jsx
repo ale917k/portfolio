655ecfec804e632692aceb6f38298d984d9198c9
ale917k <alessiopetrin9@gmail.com>
2020-09-13 16:16:27 +0100

    Implemented custom Scrollbar
    Updated and completed Scrollbar used on MiniDrawer on Skills page

--- client/src/components/global/Scrollbar.component.jsx
+++ client/src/components/global/Scrollbar.component.jsx
@@ -1,10 +1,184 @@
-import React from 'react';
+import React from "react";
 import SimpleBarReact from "simplebar-react";
 
-import "simplebar/src/simplebar.css";
-    
-const Scrollbar = () => {
-    return <SimpleBarReact style={{ maxHeight: '100vh' }} />
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  scrollbar: {
+    width: "100%",
+    maxHeight: "100vh",
+    backgroundColor: theme.palette.common.white,
+    position: "relative",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignContent: "flex-start",
+    alignItems: "flex-start",
+    "& .simplebar-wrapper": {
+      overflow: "hidden",
+      width: "inherit",
+      height: "inherit",
+      maxWidth: "inherit",
+      maxHeight: "inherit",
+    },
+    "& .simplebar-mask": {
+      direction: "inherit",
+      position: "absolute",
+      overflow: "hidden",
+      padding: 0,
+      margin: 0,
+      left: 0,
+      top: 0,
+      bottom: 0,
+      right: 0,
+      width: "auto !important",
+      height: "auto !important",
+      zIndex: 0,
+    },
+    "& .simplebar-offset": {
+      direction: "inherit !important",
+      boxSizing: "inherit !important",
+      resize: "none !important",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      padding: 0,
+      margin: 0,
+      webkitOverflowScrolling: "touch",
+    },
+    "& .simplebar-content-wrapper": {
+      direction: "inherit",
+      boxSizing: "border-box !important",
+      position: "relative",
+      display: "block",
+      height:
+        "100%" /* Required for horizontal native scrollbar to not appear if parent is taller than natural height */,
+      width: "auto",
+      maxWidth: "100%" /* Not required for horizontal scroll to trigger */,
+      maxHeight: "100%" /* Needed for vertical scroll to trigger */,
+      scrollbarWidth: "none",
+      msOverflowStyle: "none",
+    },
+    "& .simplebar-content-wrapper::-webkit-scrollbar, & .simplebar-hide-scrollbar::-webkit-scrollbar": {
+      width: 0,
+      height: 0,
+    },
+    "& .simplebar-content:before, & .simplebar-content:after": {
+      content: "''",
+      display: "table",
+    },
+    "& .simplebar-placeholder": {
+      maxHeight: "100%",
+      maxWidth: "100%",
+      width: "100%",
+      pointerEvents: "none",
+    },
+    "& .simplebar-height-auto-observer-wrapper": {
+      boxSizing: "inherit !important",
+      height: "100%",
+      width: "100%",
+      maxWidth: 1,
+      position: "relative",
+      float: "left",
+      maxHeight: 1,
+      overflow: "hidden",
+      zIndex: -1,
+      padding: 0,
+      margin: 0,
+      pointerEvents: "none",
+      flexGrow: "inherit",
+      flexShrink: 0,
+      flexBasis: 0,
+    },
+    "& .simplebar-height-auto-observer": {
+      boxSizing: "inherit",
+      display: "block",
+      opacity: 0,
+      position: "absolute",
+      top: 0,
+      left: 0,
+      height: "100%",
+      width: "1000%",
+      minHeight: 1,
+      minWidth: 1,
+      overflow: "hidden",
+      pointerEvents: "none",
+      zIndex: -1,
+    },
+    "& .simplebar-track": {
+      zIndex: 1,
+      position: "absolute",
+      right: 0,
+      bottom: 0,
+      pointerEvents: "none",
+      overflow: "hidden",
+      "&.simplebar-vertical": {
+        top: 0,
+        width: theme.spacing(1.5),
+        "& .simplebar-scrollbar:before": {
+          top: 2,
+          bottom: 2,
+        },
+      },
+    },
+    "&.simplebar-dragging .simplebar-content": {
+      pointerEvents: "none",
+      userSelect: "none",
+      webkitUserSelect: "none",
+    },
+    "&.simplebar-dragging .simplebar-track": {
+      pointerEvents: "all",
+    },
+    "& .simplebar-scrollbar": {
+      position: "absolute",
+      left: 0,
+      right: 0,
+      minHeight: theme.spacing(1),
+      "&:before": {
+        position: "absolute",
+        content: "''",
+        background: theme.palette.common.black,
+        borderRadius: theme.spacing(1),
+        left: 2,
+        right: 2,
+        opacity: 0,
+        transition: theme.transitions.create(["opacity"], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+      },
+      "&.simplebar-visible:before": {
+        /* When hovered, remove all transitions from drag handle */
+        opacity: 0.5,
+        transition: theme.transitions.create(["opacity"], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+      },
+      "& .hs-dummy-scrollbar-size": {
+        direction: "rtl",
+        position: "fixed",
+        opacity: 0,
+        visibility: "hidden",
+        height: "500px",
+        width: "500px",
+        overflowY: "hidden",
+        overflowX: "scroll",
+      },
+    },
+  },
+}));
+
+const Scrollbar = ({ className, children }) => {
+  const classes = useStyles();
+
+  return (
+    <SimpleBarReact className={`${className} ${classes.scrollbar}`}>
+      {children}
+    </SimpleBarReact>
+  );
 };
 
 export default Scrollbar;
--- client/src/components/skills/MiniDrawer.component.jsx
+++ client/src/components/skills/MiniDrawer.component.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import clsx from "clsx";
-import SimpleBarReact from "simplebar-react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
@@ -26,6 +25,8 @@ import BackupRoundedIcon from "@material-ui/icons/BackupRounded";
 import CachedRoundedIcon from "@material-ui/icons/CachedRounded";
 import UpdateRoundedIcon from "@material-ui/icons/UpdateRounded";
 
+import Scrollbar from "../global/Scrollbar.component";
+
 const navLinks = [
   { title: "Frontend", Icon: LineStyleRoundedIcon },
   { title: "Backend", Icon: StorageRoundedIcon },
@@ -38,6 +39,18 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   miniDrawer: {
     display: "flex",
+  },
+  scrollbar: {
+    marginLeft: 70,
+    width: `calc(100% - 70px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  scrollbarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
@@ -95,18 +108,14 @@ const useStyles = makeStyles((theme) => ({
     position: "absolute",
     right: theme.spacing(2),
   },
-  scrollbar: {
-    maxHeight: '100vh',
-    backgroundColor: theme.palette.common.white,
-  },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    height: "100vh",
+    height: "100%",
   },
 }));
 
-const MiniDrawer = (props) => {
+const MiniDrawer = ({ children }) => {
   const classes = useStyles();
   const [open, setOpen] = useState(true);
 
@@ -121,98 +130,102 @@ const MiniDrawer = (props) => {
   return (
     <div className={classes.miniDrawer}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
+      <Scrollbar
+        className={clsx(classes.scrollbar, {
+          [classes.scrollbarShift]: open,
         })}
       >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Skills
-          </Typography>
-          <IconButton
-            color="inherit"
-            aria-label="close skills"
-            edge="start"
-            component={RouterLink}
-            to="/about"
-            className={classes.closeSkills}
-          >
-            <CloseRoundedIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Skills
+            </Typography>
+            <IconButton
+              color="inherit"
+              aria-label="close skills"
+              edge="start"
+              component={RouterLink}
+              to="/about"
+              className={classes.closeSkills}
+            >
+              <CloseRoundedIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {navLinks.map(({ title, Icon }) => (
-            <ListItem
-              button
-              key={title}
-              component={RouterLink}
-              to={`/skills/${title.toLowerCase()}`}
-            >
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {navLinks.map(({ title, Icon }) => (
+              <ListItem
+                button
+                key={title}
+                component={RouterLink}
+                to={`/skills/${title.toLowerCase()}`}
+              >
+                <ListItemIcon>
+                  <Icon />
+                </ListItemIcon>
+                <ListItemText primary={title} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            <ListItem button component={RouterLink} to="/skills/progress">
               <ListItemIcon>
-                <Icon />
+                <CachedRoundedIcon />
               </ListItemIcon>
-              <ListItemText primary={title} />
+              <ListItemText primary="Progress" />
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          <ListItem button component={RouterLink} to="/skills/progress">
-            <ListItemIcon>
-              <CachedRoundedIcon />
-            </ListItemIcon>
-            <ListItemText primary="Progress" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem button component={RouterLink} to="/skills/next">
-            <ListItemIcon>
-              <UpdateRoundedIcon />
-            </ListItemIcon>
-            <ListItemText primary="What's Next" />
-          </ListItem>
-        </List>
-      </Drawer>
-      <SimpleBarReact className={classes.scrollbar}>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button component={RouterLink} to="/skills/next">
+              <ListItemIcon>
+                <UpdateRoundedIcon />
+              </ListItemIcon>
+              <ListItemText primary="What's Next" />
+            </ListItem>
+          </List>
+        </Drawer>
         <main className={classes.content}>
           <div className={classes.toolbar} />
-          {props.children}
+          {children}
         </main>
-      </SimpleBarReact>
+      </Scrollbar>
     </div>
   );
 };
